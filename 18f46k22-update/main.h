72456d2f10e1782774688d9c8801c263864d1018
Furkan Bakkal <furkanbakkal@outlook.com.tr>
2022-09-04 16:33:08 +0300

    Add files via upload
    Centering algorithm changed
    Optimization for 18F46K22
    Bug fixes

---
+++ 18f46k22-update/main.h
@@ -0,0 +1,9 @@
+#include <18F46K22.h>
+#device ADC=10
+
+#fuses NOWDT      //No Watch Dog Timer
+
+#use delay(internal=64MHz)
+#use timer(timer=0,tick=100us,bits=32,NOISR)
+#define TICK_TYPE unsigned int32
+
